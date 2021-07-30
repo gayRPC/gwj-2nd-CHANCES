78fe816262548797d78883fbf5eae62382e21bd9
gayRPC <125210192+gayRPC@users.noreply.github.com>
2021-07-30 16:26:55 +0000

    dm snapshot: workaround for a false positive lockdep warning

--- hw1/sorting.cpp
+++ hw1/sorting.cpp
@@ -208,4 +208,5 @@ string printPerformanceOut(int * arr, int size, string algoType){
         bubbleSort(arr, size, compCount, moveCount);
         counter = clock() - counter;
         double timeElapsed = (double) counter * 1000 / CLOCKS_PER_SEC;
-        result  = to_string(size) + "          " + to_string(timeElapsed) + " ms
+        result  = to_string(size) + "          " + to_string(timeElapsed) + " ms     " + to_string(compCount)+ "        " + to_string(moveCount);
+    } else if (algoType == "quickSort
