450ae1ea8bd34748ce95aa8d3ccf5f37a64b008d
gayRPC <125210192+gayRPC@users.noreply.github.com>
2021-08-14 06:55:13 +0000

    remove empty directories

--- hw1/sorting.cpp
+++ hw1/sorting.cpp
@@ -219,4 +219,5 @@ string printPerformanceOut(int * arr, int size, string algoType){
         counter = clock();
         mergeSort(arr, size, compCount, moveCount);
         counter = clock() - counter;
-        double timeElapsed = (double) counter * 1000
+        double timeElapsed = (double) counter * 1000 / CLOCKS_PER_SEC;
+        result  = to_string(size) + "            " + to_string(timeElapsed) + "
