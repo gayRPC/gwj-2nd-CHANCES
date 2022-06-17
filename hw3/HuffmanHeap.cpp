ac6e7b94de43324ebe49a55a6368d0ee1f20ff3c
gayRPC <125210192+gayRPC@users.noreply.github.com>
2022-06-17 05:41:37 +0000

    Dockerfile derp

--- hw3/HuffmanHeap.cpp
+++ hw3/HuffmanHeap.cpp
@@ -40,3 +40,98 @@ int HuffmanHeap::peek() {
 }
 
 // insert a new value into the heap
+void HuffmanHeap::insert(MinHeapNode *& nodePtr){
+    // 0) Check whether there is a space left for inserting the value
+    // 1) Appand the new value at the end of the array
+    // 2) Go up to by checking parents and max heap condition
+    if (size() >= MAX_SIZE_HC) {
+        // 0) reached to the maximum size
+        cout << "ERROR: exceeds max capacity of heap!" << endl;
+        return;
+    } else {
+        // place the item at the end
+        items[sizeHeap] = nodePtr;
+        
+        // remember the place and move the item to the top
+        int pos = size();
+        
+        int parent = (pos -1)/2;
+        while ((pos > 0) && ((*items[pos]).freq < (*items[parent]).freq)) {
+            // swap elements if parent is smaller than the current element
+            // no dynamically allocated data so no leak problem
+            MinHeapNode *temp =  items[parent];
+            items[parent] = items[pos];
+            items[pos] = temp;
+            
+            // move current node and parent up
+            pos = parent;
+            parent = (pos - 1)/2;
+        }
+        // increase the size
+        sizeHeap++;
+    }
+}
+
+
+
+// retrieves and deletes max element from the heap
+HuffmanHeap::MinHeapNode* HuffmanHeap::extractMin(){
+    if (isEmpty()) {
+        // heap is empty
+        return NULL;
+    } else {
+        // get the result pointer
+        MinHeapNode *minItem;
+        // call heapDelete function
+        heapDelete(minItem);
+        // return the result
+        return minItem;
+    }
+}
+
+
+// Creates node and inserts the node in the heap
+void HuffmanHeap::insert(const int freq, const char character){
+    // 0) Check whether there is a space left for inserting the value
+    // 1) Appand the new value at the end of the array
+    // 2) Go up to by checking parents and max heap condition
+    if (size() >= MAX_SIZE_HC) {
+        // 0) reached to the maximum size
+        cout << "ERROR: exceeds max capacity of heap!" << endl;
+        return;
+    } else {
+        MinHeapNode *nodePtr = new MinHeapNode;
+        nodePtr->freq = freq;
+        nodePtr->character = character;
+        nodePtr->left = NULL;
+        nodePtr->right = NULL;
+        // place the item at the end
+        items[sizeHeap] = nodePtr;
+        
+        // remember the place and move the item to the top
+        int pos = size();
+        
+        int parent = (pos -1)/2;
+        while ((pos > 0) && ((*items[pos]).freq < (*items[parent]).freq)) {
+            // swap elements if parent is smaller than the current element
+            // no dynamically allocated data so no leak problem
+            MinHeapNode *temp =  items[parent];
+            items[parent] = items[pos];
+            items[pos] = temp;
+            
+            // move current node and parent up
+            pos = parent;
+            parent = (pos - 1)/2;
+        }
+        // increase the size
+        sizeHeap++;
+    }
+}
+
+// ========== PRIVATE HELPER FUNCTIONS =============== //////
+
+// HeapRebuild: satisfy heap property again
+void HuffmanHeap::heapRebuild(int root){
+    int childPtr = (root * 2) + 1;
+    if ( childPtr < sizeHeap) {
+        // not a leaf situation check existance of right child
