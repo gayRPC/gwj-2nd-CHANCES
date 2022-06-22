b40846aff78c0d4897338e185452a43873fdb818
gayRPC <125210192+gayRPC@users.noreply.github.com>
2022-06-22 14:37:09 +0000

    Added travis.yml.

--- hw3/HuffmanQueue.cpp
+++ hw3/HuffmanQueue.cpp
@@ -4,4 +4,5 @@
  * ID: 21703381
  * Section : 2
  * Assignment : 3
- * Description : Source (.cpp) file 
+ * Description : Source (.cpp) file for min priority queue which will be used for Huffman Coding
+ *              - Abstraction of a priority queu
