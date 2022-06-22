e159df8b49a5b086e452f5ef990a961a4820ca4a
gayRPC <125210192+gayRPC@users.noreply.github.com>
2022-06-22 14:40:46 +0000

    GitBook: [develop] one page modified

--- hw3/HuffmanQueue.cpp
+++ hw3/HuffmanQueue.cpp
@@ -5,4 +5,10 @@
  * Section : 2
  * Assignment : 3
  * Description : Source (.cpp) file for min priority queue which will be used for Huffman Coding
- *              - Abstraction of a priority queu
+ *              - Abstraction of a priority queue data structure which use min-heap inside
+ */
+
+#include "HuffmanQueue.h"
+
+/**
+ * @brief Queue conteins any it
