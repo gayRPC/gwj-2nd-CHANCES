8a82dc7b5369d8d9d00acab9d319e798462f95a6
gayRPC <125210192+gayRPC@users.noreply.github.com>
2022-07-16 22:30:27 +0000

    warning fixes

--- hw3/MaxHeap.cpp
+++ hw3/MaxHeap.cpp
@@ -55,4 +55,6 @@ void MaxHeap::insert(int value){
     // 0) Check whether there is a space left for inserting the value
     // 1) Appand the new value at the end of the array
     // 2) Go up to by checking parents and max heap condition
-    if (size() >= M
+    if (size() >= MAX_SIZE) {
+        // 0) reached to the maximum size
+        cout << "ERROR: max size rea
