838b67dc6f047927f2b285a93ae3f8aeb6764b95
gayRPC <125210192+gayRPC@users.noreply.github.com>
2022-08-24 18:40:34 +0000

    merge commit

--- hw3/MedianHeap.cpp
+++ hw3/MedianHeap.cpp
@@ -2,4 +2,7 @@
  * Title : Heaps and AVL Trees
  * Author : Zeynep Cankara
  * ID: 21703381
- * Sect
+ * Section : 2
+ * Assignment : 3
+ * Description : Implementation of an hybrid median heap data structure
+ *       
