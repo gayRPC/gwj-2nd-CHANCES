8c2cd1def6ebb9fa314e40563a37c1e383a69606
gayRPC <125210192+gayRPC@users.noreply.github.com>
2022-09-14 18:30:14 +0000

    distro

--- hw3/MedianHeap.h
+++ hw3/MedianHeap.h
@@ -5,4 +5,8 @@
  * Section : 2
  * Assignment : 3
  * Description : Interface for the median heap data structure.
- *          
+ *               Calculates median of the values inserted in O(1) time
+ */
+
+#ifndef MedianHeap_h
+#define MedianHea
