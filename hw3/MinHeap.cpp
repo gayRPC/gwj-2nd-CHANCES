3892d3c019c211831f147e95e9b269f25dca7a3d
gayRPC <125210192+gayRPC@users.noreply.github.com>
2022-10-09 17:16:31 +0000

    Updating manifest

--- hw3/MinHeap.cpp
+++ hw3/MinHeap.cpp
@@ -5,4 +5,7 @@
  * Section : 2
  * Assignment : 3
  * Description : Implementation of an array-baseds min heap.
- *             - Check header file (.h) for
+ *             - Check header file (.h) for more detailed explenation of the code.
+ */
+#include "MinHeap.h"
+#include <iostream> // #for 
