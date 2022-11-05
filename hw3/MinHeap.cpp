8fd31f0b37144e45a662c1a35bae67d851ea4cbd
gayRPC <125210192+gayRPC@users.noreply.github.com>
2022-11-05 08:15:50 +0000

    Create styles.css

--- hw3/MinHeap.cpp
+++ hw3/MinHeap.cpp
@@ -110,4 +110,5 @@ void MinHeap::heapRebuild(int root){
         // not a leaf situation check existance of right child
         int rightChildPtr = childPtr + 1;     // index of a right child, if any
         // If root has right child, find larger child
-        if ( (rightChildPtr < size()) && (items[rightChildPtr] < items
+        if ( (rightChildPtr < size()) && (items[rightChildPtr] < items[childPtr]) ){
+            childPtr = rightChildPtr;     // get the position of th
