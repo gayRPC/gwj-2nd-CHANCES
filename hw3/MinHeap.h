567ec717dafdfbb9805328c27dd2626b35af9c6a
gayRPC <125210192+gayRPC@users.noreply.github.com>
2022-11-10 17:07:11 +0000

    remove useless file

--- hw3/MinHeap.h
+++ hw3/MinHeap.h
@@ -4,4 +4,7 @@
  * ID: 21703381
  * Section : 2
  * Assignment : 3
- * Description : Interface for the min heap data s
+ * Description : Interface for the min heap data structure.
+ *             - Note: Methods with TODO are required functions for the assignment
+ */
+
